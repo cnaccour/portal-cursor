ad54ef75f6d82c2eca216b9aede8193cfd96711a
Christian Naccour <chris@Christians-MacBook-Pro.local>
2025-09-26 14:52:19 -0400

    Fix notifications: force notification manager to use database instead of mock mode

--- public/includes/notification-manager.php
+++ public/includes/notification-manager.php
@@ -9,8 +9,8 @@ class NotificationManager {
     private $use_mock = true; // Switch to false when real database is available
     
     private function __construct() {
-        // Check if we should use real database or mock
-        $this->use_mock = !$this->isDatabaseAvailable();
+        // Force use of database since we know it's working
+        $this->use_mock = false;
     }
     
     public static function getInstance() {
